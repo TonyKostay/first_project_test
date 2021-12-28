49fc70bb63973895c87734d57290808a5f31e1a0
TonyKostay <tonykostay@yandex.ru>
2021-12-28 22:13:09 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+//let number = 4.6;
+
+//const persone = "Alex";
+//const bool = true;
+
+//const obj = {
+//    name: "John",
+//    age: 25
+//};
+//console.log(obj.name);
+
+//let arr = ['plum.png', 'orange.jpg'];
+//console.log(arr[0]);
+
+//alert('Hello');
+//const result = confirm("are you tut blya?");
+//onst answer = +prompt('Вам есть 18?',"18");
+//console.log(typeof(answer));
+//const answers = [];
+//answers[0]=prompt('Как ваше имя?');
+//answers[3]=prompt('Как ваша фамилия?');
+//answers[3]=prompt('Сколько вам лет?');
+
+//console.log(typeof(answers));
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`)
+
+//const user = 'Ivan';
+//alert(`Hello, ${user}`);
+
+//console.log('arr'+"- object");
+//console.log(7+"- object");
+//console.log(6+ +"6");
+
+//let incr = 10,
+    //decr = 10;
+//incr++;           //постфиксная форма (сначала возвращает значение, а потом увеличивает/уменьшает на единицу)
+//++decr;           //префиксная форма (сначала увеличивает/уменьшает, затем возвращает)
+//console.log(++incr);
+//console.log(decr--);
+
+//console.log(2*4 ==  '8');  //не строгое равенство, сравнивает значения переменных
+//console.log(2*4 ===  '8'); //строгое равенство, смотрит и на тип данных
+// && - логическое И
+// || - логическое ИЛИ
+let price = 20;
+let discont = 25;
+let total = price - (price*(discont/1000));
+alert(total);
